54bf9f5d2ea5eead2767b9d9f90ebb5d77d70a95
12tae12 <84397906+12tae12@users.noreply.github.com>
2024-07-03 21:28:14 +0000

    assdsadadasda

--- server.js
+++ server.js
@@ -20,5 +20,6 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(listening on *:${PORT});
+    console.log(`listening on *:${PORT}`);
 });
+
