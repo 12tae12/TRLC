accb86b63769cfff06649e61ce3812c3be809de5
12tae12 <84397906+12tae12@users.noreply.github.com>
2024-07-03 22:22:11 +0000

    I did it
    yay

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ let chatMessages = [];
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    // Send chat history only on connection
+    socket.emit('chat history', chatMessages);
     socket.on('chat message', (msg) => {
         // Add the message to the array
         chatMessages.push(msg);
         // Emit the message to all connected clients
-        io.emit('chat message', msg);
-        // Also emit the entire chat history to the new user
-        socket.emit('chat history', chatMessages);
+        io.emit('chat message', msg); 
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
